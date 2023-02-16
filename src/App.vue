2a619ea6dae8b18fe4e4f07a3cff0c3b2fd30b81
Tyoukai <zhang_k_w@163.com>
2023-02-16 22:45:43 +0800

    更新组件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,49 @@
 <template>
   <el-container>
-    <el-header><Header/></el-header>
-    <el-main><Main/></el-main>
+    <el-aside>
+      <el-menu>
+        <el-menu-item index="1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航一</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航五</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航六</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 16px">
+        <span>王小虎</span>
+      </el-header>
+      <el-main>
+        <el-table :data="tableData">
+        <el-table-column prop="date" label="日期" width="140">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="120">
+        </el-table-column>
+        <el-table-column prop="address" label="地址">
+        </el-table-column>
+      </el-table>
+      </el-main>
+    </el-container>
   </el-container>
 </template>
 
@@ -9,13 +51,24 @@
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
 
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Main
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Main
+    },
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(7).fill(item)
+      }
+    }
   }
-}
+
 </script>
 
 <style>
@@ -27,4 +80,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
 </style>
