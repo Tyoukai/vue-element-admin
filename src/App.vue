99f6741f3a2786454450044a4188f1e1fe88086a
Tyoukai <zhang_k_w@163.com>
2023-03-06 19:11:17 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,7 @@ import Main from './components/Main.vue';
     },
     data() {
       const item = {
-        date: '2016-05-02',
+        date: '2016-05-02' ,
         name: '王小虎',
         address: '上海市普陀区金沙江路 1518 弄'
       };
