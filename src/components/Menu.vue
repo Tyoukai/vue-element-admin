ada8c1998c6a35c6abf49f4bc60f255b9e1d60f6
Tyoukai <zhang_k_w@163.com>
2023-03-14 20:17:18 +0800

    后台管理页面纯html

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,7 +3,7 @@
       <el-menu>
         <el-menu-item index="1">
           <i class="el-icon-menu"></i>
-          <span slot="title">导航一</span>
+          <span slot="title">kafka相关</span>
         </el-menu-item>
         <el-menu-item index="2">
           <i class="el-icon-menu"></i>
