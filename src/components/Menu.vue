b8b6529ef9fa2a395545256658757f48f20bd241
Tyoukai <zhang_k_w@163.com>
2023-03-25 21:29:52 +0800

    some update

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="clo-md-2 sidebar">
     <ul class="nav nav-sidebar">
-      <li class="active"><router-link to="/home">首页</router-link></li>
-      <li><router-link to="/kafkaConfig">kafka配置</router-link></li>
-      <li><router-link to="/robotConfig">外挂机器人配置</router-link></li>
-      <li><router-link to="/watchdogConfig">看门狗配置</router-link></li>
+      <li :class="activeData.home"><router-link to="/home">首页</router-link></li>
+      <li :class="activeData.kafka"><router-link to="/kafkaConfig">kafka配置</router-link></li>
+      <li :class="activeData.robot"><router-link to="/robotConfig">外挂机器人配置</router-link></li>
+      <li :class="activeData.watchdog"><router-link to="/watchdogConfig">看门狗配置</router-link></li>
     </ul>
   </div>
 </template>
@@ -12,6 +12,41 @@
 <script>
 export default {
   name: "Menu",
+  data() {
+    return {
+      activeData: {
+        home: 'active',
+        kafka: '',
+        robot: '',
+        watchdog: ''
+      }
+    }
+  },
+  methods: {
+    changeCss(item){
+      if (item === 'home') {
+        this.data.activeData.home = 'active'
+        this.data.activeData.kafka = ''
+        this.data.activeData.robot = ''
+        this.data.activeData.watchdog = ''
+      } else if (item === 'kafka') {
+        this.data.activeData.home = ''
+        this.data.activeData.kafka = 'active'
+        this.data.activeData.robot = ''
+        this.data.activeData.watchdog = ''
+      } else if (item === 'robot') {
+        this.data.activeData.home = ''
+        this.data.activeData.kafka = ''
+        this.data.activeData.robot = 'active'
+        this.data.activeData.watchdog = ''
+      } else {
+        this.data.activeData.home = ''
+        this.data.activeData.kafka = ''
+        this.data.activeData.robot = ''
+        this.data.activeData.watchdog = 'active'
+      }
+    }
+  }
 }
 </script>
 
